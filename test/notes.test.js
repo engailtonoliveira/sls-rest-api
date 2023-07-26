02412486e931a6fd89861d79359b090f2941fd29
engailtonoliveira <engailtgonoliveira@gmail.com>
2023-07-25 22:47:53 -0300

    add unit testing

---
+++ test/notes.test.js
@@ -0,0 +1,20 @@
+'use strict'
+let init = require('./steps/init');
+let { an_authenticated_user } = require('./steps/given');
+let idToken;
+
+describe(`Given an authenticated user`, () => {
+    beforeAll( async () => {
+        init();
+        let user = await an_authenticated_user();
+        idToken = user.AuthenticationResult.IdToken;
+        console.log(idToken);
+    }); 
+
+
+    describe(`When we invoke Post /notes endpoint`, () => {
+        it("Should create a new note", async () => {
+            expect(true).toBe(True);
+        })
+    });
+});
